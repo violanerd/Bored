4e7906a72f85e315bb9e8acb5af35e6e94d6fe43
violanerd <madalyne.cross@gmail.com>
2022-06-28 20:28:45 -0500

    a fun little project

---
+++ script.js
@@ -0,0 +1,20 @@
+var buttonEl = document.getElementById("the-button");
+
+
+function getNewActivity () { 
+
+    var url = "http://www.boredapi.com/api/activity/";
+
+    var displayEl = document.getElementById("display-data");
+
+    fetch(url)
+        .then(function(response) {
+            // Convert to JSON object
+            return response.json();
+        })
+        .then(function(data) {
+            var activity = data.activity;
+            displayEl.textContent=activity;
+        })
+}
+buttonEl.addEventListener("click", getNewActivity);
